35a16a091762b357722ffcbf9df06ee78561f9c5
Dominik Stolz <dominik.stolz21@gmail.com>
2016-01-23 18:53:03 +0100

    Umleitung Authentifizierter Benutzer auf der Anmeldeseite

--- src/main/java/de/gymwak/gwe/mvc/LoginController.java
+++ src/main/java/de/gymwak/gwe/mvc/LoginController.java
@@ -1,5 +1,8 @@
 package de.gymwak.gwe.mvc;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,14 @@ public class LoginController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String login() {
+		if (authenticated()) {
+			return "redirect:/user";
+		}
 		return "login";
+	}
+
+	private boolean authenticated() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return auth != null && !(auth instanceof AnonymousAuthenticationToken) && auth.isAuthenticated();
 	}
 }
