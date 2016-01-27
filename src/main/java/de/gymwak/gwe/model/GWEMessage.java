95053bedfdef381d4851567da16758076d34c8e6
0liCom <0liCom@users.noreply.github.com>
2016-01-27 08:23:27 +0100

    Feedback teilweise hinzugefügt
    + Textbox zum Versenden einer Nachricht
    + Neue Methode im ImpressumController
    # Design überarbeitet (von abgerundeten Ecken zu scharfen Ecken,
    Abhebung vom Standard-Bootstrap-Design für mehr Harmonierung der panels
    mit Footer und Navigations-Kasten)
    + Neue Klasse GWEMessage für Nachrichten
    
    Legende:
    + Hinzugefügt
    # Geändert
    - Entfernt
    
    Anmerkungen:
    
    - Feedback kann/sollte in diesem Stand eventuell in eine eigene Seite
    eingebaut werden
    - Abgegebene Feedback-Nachrichten werden noch nicht gespeichert

---
+++ src/main/java/de/gymwak/gwe/model/GWEMessage.java
@@ -0,0 +1,68 @@
+package de.gymwak.gwe.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class GWEMessage implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Email(message = "Please provide a valid email address.")
+	@NotEmpty(message = "Email is required.")
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@NotEmpty(message = "First name is required.")
+	private String message;
+
+	public GWEMessage() { }
+	
+	public GWEMessage(GWEMessage message) {
+		this.id = message.getId();
+		this.email = message.getEmail();
+		this.message = message.getMessage();
+	}
+
+	public GWEMessage(String email, String message) {
+		this.email = email;
+		this.message = message;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	private static final long serialVersionUID = 822638085876787423L;
+
+}
--- src/main/java/de/gymwak/gwe/mvc/ImpressumController.java
+++ src/main/java/de/gymwak/gwe/mvc/ImpressumController.java
@@ -1,8 +1,13 @@
 package de.gymwak.gwe.mvc;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.gymwak.gwe.model.GWEMessage;
 
 @Controller
 @RequestMapping("/impressum")
@@ -11,6 +16,22 @@ public class ImpressumController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String get() {
 		return "impressum";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String reportBug(@Valid GWEMessage message, BindingResult result) {
+		if (result.hasErrors()) {
+			return "redirect:/impressum?error#feedback";
+		}
+		
+		if (message.getMessage().isEmpty()) {
+			return "redirect:/impressum?error=message#feedback";
+		}
+		
+		// TODO Daten in eine (temporäre?) Datenbank(?) speichern
+		
+		return "redirect:/impressum?action=success#feedback";
+
 	}
 
 }
