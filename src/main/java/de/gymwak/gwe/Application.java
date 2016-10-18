93b2aa369c277ecb6905e09075246d6718e010ec
Dominik Stolz <dominik.stolz21@gmail.com>
2016-10-18 21:01:17 +0200

    Verwendung des eingebetteten Tomcat Containers

--- src/main/java/de/gymwak/gwe/Application.java
+++ src/main/java/de/gymwak/gwe/Application.java
@@ -3,15 +3,9 @@ package de.gymwak.gwe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+public class Application {
 
 	/**
 	 * Diese main Methode startet beim Ausfuehren einen Tomcat Server auf dem
