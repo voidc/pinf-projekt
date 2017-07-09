2cedfe7db5491215575b4c3e45d9c3c41bb31064
0liCom <oli.com@t-online.de>
2017-07-09 22:01:06 +0200

    Nutzer löschen-Funktion hinzugefügt

--- src/main/java/de/gymnasiumwaldkraiburg/gwe/mvc/AdminController.java
+++ src/main/java/de/gymnasiumwaldkraiburg/gwe/mvc/AdminController.java
@@ -1,8 +1,10 @@
 package de.gymnasiumwaldkraiburg.gwe.mvc;
 
 import de.gymnasiumwaldkraiburg.gwe.data.GWEBannerRepository;
+import de.gymnasiumwaldkraiburg.gwe.data.GWEEventRepository;
 import de.gymnasiumwaldkraiburg.gwe.data.GWERepository;
 import de.gymnasiumwaldkraiburg.gwe.model.GWEBanner;
+import de.gymnasiumwaldkraiburg.gwe.model.GWEEvent;
 import de.gymnasiumwaldkraiburg.gwe.model.GWEUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -15,16 +17,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
     private GWERepository userRepository;
     private GWEBannerRepository bannerRepository;
+    private GWEEventRepository eventRepository;
 
     @Autowired
-    public AdminController(GWERepository userRepository, GWEBannerRepository bannerRepository) {
+    public AdminController(GWERepository userRepository, GWEBannerRepository bannerRepository, GWEEventRepository eventRepository) {
         this.userRepository = userRepository;
         this.bannerRepository = bannerRepository;
+        this.eventRepository = eventRepository;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -47,7 +53,7 @@ public class AdminController {
         }
     }
 
-    @RequestMapping(method = RequestMethod.POST, params = {"update"})
+    @RequestMapping(method = RequestMethod.POST, params = {"updateBanner"})
     public String updateBanner(@RequestParam(value = "content", required = true) String content,
                                @RequestParam String color, @RequestParam(required = false) boolean dismissible) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -72,7 +78,7 @@ public class AdminController {
         return "redirect:/admin#banner";
     }
 
-    @RequestMapping(method = RequestMethod.POST, params = {"delete"})
+    @RequestMapping(method = RequestMethod.POST, params = {"deleteBanner"})
     public String deleteBanner() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         GWEUser currentUser = userRepository.findByEmail(auth.getName());
@@ -86,7 +92,43 @@ public class AdminController {
         return "redirect:/admin#banner";
     }
 
-    @RequestMapping(value = "/bannerClose", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.POST, params = {"deleteUser"})
+    public String deleteUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam int gradYear) {
+        System.out.println("success?");
+
+        List<GWEUser> users = userRepository.findByGraduationYear(gradYear);
+        GWEUser deleteUser = null;
+
+        for (GWEUser user : users) {
+            // TODO multiple users of same name/gradYear seperation
+
+            if (!user.isAdmin() && user.getFirstName().equals(firstName) && user.getLastName().equals(lastName)) {
+                deleteUser = user;
+            }
+        }
+
+        if (deleteUser == null) {
+            return "redirect:/admin?delete=error#deleteUser";
+        } else {
+            eventRepository.delete(eventRepository.findByOrganizerId(deleteUser.getId()));
+
+            Iterable<GWEEvent> events = eventRepository.findAll();
+            for (GWEEvent e : events) {
+                if (e.hasParticipant(deleteUser)) {
+                    List<GWEUser> p = e.getParticipants();
+                    p.remove(deleteUser);
+                    e.setParticipants(p);
+                    eventRepository.save(e);
+                }
+            }
+
+            userRepository.delete(deleteUser);
+
+            return "redirect:/admin?delete=success#deleteUser";
+        }
+    }
+
+    @RequestMapping(value = "/closeBanner", method = RequestMethod.GET)
     public String bannerClose() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         GWEUser currentUser = userRepository.findByEmail(auth.getName());
